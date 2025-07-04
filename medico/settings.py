10392d9b4572a06ad7d17cb0751d9989f6abb91a
Danny Simba <dannysimba2602@gmail.com>
2025-07-04 12:31:53 -0500

    render sin clipsy

--- medico/settings.py
+++ medico/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Para collectstatic
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'medico/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, '/medico/static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
